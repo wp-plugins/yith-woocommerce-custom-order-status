e1dfc1ff8856b9c7ed1e88010974c5fccea0a2ba
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 08:04:53 +0000

    Version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/yith-woocommerce-custom-order-status/tags/1.0.2@1220699 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-options/premium-options.php
@@ -0,0 +1,13 @@
+<?php
+
+// Exit if accessed directly
+! defined( 'YITH_WCCOS' )  && exit();
+
+return array(
+	'premium' => array(
+		'landing' => array(
+			'type' => 'custom_tab',
+			'action' => 'yith_wccos_premium_tab'
+		)
+	)
+);
